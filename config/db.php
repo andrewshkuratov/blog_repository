f2dc6ea0d34624ff7ea29a55b88b8dc3c9d5b3bb
KiptenkoBogdan <bogdankiptenko@gmail.com>
2023-01-12 21:26:14 +0200

    Done "Use migration" Fig.1.23-1.28

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=blog',
+    'dsn' => 'mysql:host=localhost;dbname=yiiproject',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
