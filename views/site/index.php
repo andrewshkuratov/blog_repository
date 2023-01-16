4684b9b325c18db8fc0cffbab600c2dfeda46780
KiptenkoBogdan <bogdankiptenko@gmail.com>
2023-01-16 11:39:30 +0200

    Added home page
    Added homepage

--- views/site/index.php
+++ views/site/index.php
@@ -1,3 +1,38 @@
-<div class="col-md-8">
-    <h6> LETS FUCKING GO !!!!<h6>
+<div class="index-container">
+    <div class="desc-container">
+        <p class="desc-title">Get in touch with with the industry</p>
+        <p class="desc-text">Check the latest news for new releases, tours and all things music from artists across the world.</p>
+        <a href="/site/blog">
+            <img class="img-index" src="button_news.png" alt="">
+        </a>
+    </div>
+    <div class="img-container">
+        <img class="img-index" src="home1.jpg">
+    </div>
+</div>
+<div class="index-container">
+    <div class="img-container">
+        <img class="img-index" src="home2.jpg">
+    </div>
+    <div class="desc-container">
+        <p class="desc-title">Explore new things</p>
+        <p class="desc-text">Share your thoughts and get inspiration from others. 
+            Browse tons of articles about new tracks, sound libraries and audio samples from other users. 
+            Share your feedback and discuss ideas.</p>
+        <a href="/site/blog">
+            <img class="img-index" src="button_blog.png" alt="">
+        </a>
+    </div>
+</div>
+<div class="index-container">
+    <div class="desc-container">
+        <p class="desc-title">Have something to share?</p>
+        <p class="desc-text">Sign up now and start uploading.</p>
+        <a href="/site/login">
+            <img class="img-index" src="button_signup.png">
+        </a>
+    </div>
+    <div class="img-container">
+        <img class="img-index" src="home3.jpg">
+    </div>
 </div>
